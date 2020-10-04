c4a9bf054b953f1c921ba2db4da0c8312ffc9cde
MariusBrockhoff <67380126+MariusBrockhoff@users.noreply.github.com>
2020-10-04 14:37:56 +0100

    Defined first support functions and setup notebook architecture

--- unit_testing.py
+++ unit_testing.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-from utils.Fully_connected_ANN import*
+from utils.fully_connected_ANN import*
 
-data = np.loadtxt('C:/Users/Marius/Documents/GitHub/ML-Neuron-Classification/Datasets/testing/Combined_Data_ML.txt', dtype=np.float, delimiter=',')
+data = np.loadtxt('C:/Users/Marius/Documents/Datasets/testing/Combined_Data_ML.txt', dtype=np.float, delimiter=',')
 idx = np.random.choice(data.shape[0], 46769, replace=False)
 test_labels = data[idx,256]
 test_data = data[idx,0:256]
@@ -16,4 +16,4 @@ model = import_fully_connected_ANN(5, [256,256,100,100,10])
 
 model.fit(training_data, training_labels, batch_size=32, epochs=5)
 
-print("The model corrects accurately in", model.evaluate(test_data, test_labels)[1], "%")
+print("The model correctly classifies ", model.evaluate(test_data, test_labels)[1], "%")
